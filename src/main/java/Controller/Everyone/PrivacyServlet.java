6a83368dceb15f6606beb6e527cbef9929161502
michelepolverino <m.polverino9@studenti.unisa.it>
2026-04-02 15:46:03 +0200

    fatto privacy jsp , css e servlet

---
+++ src/main/java/Controller/Everyone/PrivacyServlet.java
@@ -0,0 +1,21 @@
+package Controller.Everyone;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "Privacy", value = "/Privacy")
+public class PrivacyServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Privacy.jsp");
+        dispatcher.forward(request, response);
+    }
+}
